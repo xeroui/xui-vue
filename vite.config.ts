b747cc4ada9f3608a5b00712fcb30b13d8af5111
Arafat Islam <arafatkn@gmail.com>
2024-12-15 11:46:43 +0600

    integrate and export tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { resolve } from "path";
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -8,22 +8,22 @@ export default defineConfig({
   build: {
     lib: {
       // src/index.ts is where we have exported the component(s)
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "XUIVueComponentLibrary",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'XUIVue',
       // the name of the output files when the build is run
-      fileName: "xui-vue-component-lib",
+      fileName: 'xui-vue',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+      external: ['vue'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: "Vue",
+          vue: 'Vue',
         },
       },
     },
   },
-})
+});
